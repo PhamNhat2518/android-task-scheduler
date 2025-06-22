12207d8386bb3b3784d631d80698ce7e7ca1ac2f
bdhieu2004 <buihieu274204@gmail.com>
2025-06-22 21:32:41 +0700

    Revert "Giao diện trang chủ, đăng nhập ,đăng kí, quên mật khẩu (#1)"
    This reverts commit 89dbe6065d1341bbe174bf90999e6559c9a411ef.

--- app/src/main/java/vn/edu/tlu/appquanlylichtrinh/MainActivity.java
+++ app/src/main/java/vn/edu/tlu/appquanlylichtrinh/MainActivity.java
@@ -1,37 +1,24 @@
 package vn.edu.tlu.appquanlylichtrinh;
 
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText editName;
-    private Button btnContinue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        editName = findViewById(R.id.editName);
-        btnContinue = findViewById(R.id.btnContinue);
-
-        btnContinue.setOnClickListener(v -> {
-            String name = editName.getText().toString().trim();
-            if (name.isEmpty()) {
-                Toast.makeText(MainActivity.this, "Vui lòng nhập tên!", Toast.LENGTH_SHORT).show();
-            } else if (name.length() < 2) {
-                Toast.makeText(MainActivity.this, "Tên phải có ít nhất 2 ký tự!", Toast.LENGTH_SHORT).show();
-            } else {
-                // Truyền tên sang LoginActivity
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                intent.putExtra("USER_NAME", name);
-                startActivity(intent);
-            }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
     }
 }
